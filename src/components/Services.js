5eab730f003a36e4261c55016c291ae895ec19c9
shermaine <shermaine.lim.si.hui@gmail.com>
2021-10-12 15:40:05 +0800

    Experience and Mobile Scaling

--- src/components/Services.js
+++ src/components/Services.js
@@ -9,7 +9,7 @@ const Services = () => {
       <h1 className="py-5">my services</h1>
       <div className="container">
         <div className="row">
-          <div className="col-md-3 col-sm-6">
+          <div className="col-lg-3 col-md-6 col-sm-6">
             <div className="box">
               <div className="circle">
                 <FontAwesomeIcon className="icon" icon={faDesktop} size="2x" />
@@ -22,7 +22,7 @@ const Services = () => {
             </div>
           </div>
           {/*-*/}
-          <div className="col-md-3 col-sm-6">
+          <div className="col-lg-3 col-md-6 col-sm-6">
             <div className="box">
               <div className="circle">
                 <FontAwesomeIcon className="icon" icon={faFileCode} size="2x" />
@@ -33,7 +33,7 @@ const Services = () => {
             </div>
           </div>
           {/* - */}
-          <div className="col-md-3 col-sm-6">
+          <div className="col-lg-3 col-md-6 col-sm-6">
             <div className="box">
               <div className="circle">
                 <FontAwesomeIcon
@@ -51,7 +51,7 @@ const Services = () => {
             </div>
           </div>
           {/*-*/}
-          <div className="col-md-3 col-sm-6">
+          <div className="col-lg-3 col-md-6 col-sm-6">
             <div className="box">
               <div className="circle">
                 <FontAwesomeIcon className="icon" icon={faGoogle} size="2x" />
